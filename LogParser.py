a2c3df7103f337c86b6221f7fcecbd54cbec3639
Kondratkov Artem <Kondratkov2015@yandex.ru>
2020-04-01 01:19:45 +0300

    Добавлена выборка строк, содержащих слово, передаваемое как аргумент.

--- LogParser.py
+++ LogParser.py
@@ -1,3 +1,4 @@
+import re
 import os
 from os import listdir
 from os.path import isfile, join
@@ -31,3 +32,22 @@ while incorrect_path:
 
 read_dir() if os.path.isdir(filePath) else read_file()
 all_lines = [line.rstrip() for line in all_lines]
+
+
+def mask_to_regex(mask):
+    res = mask
+    arr = ["\\", "#", "|", "(", ")", "[", "]", "{", "}", "^", "$", "+", "."]
+    for i in range(len(arr)):
+        res = str.replace(res, arr[i], "\\" + arr[i])
+    res = str.replace(res, "*", ".*")
+    res = str.replace(res, "?", ".")
+    return "^" + res + "$"
+
+
+target = input("Введите искомое слово: ")
+pattern = re.compile('.*(' + target + ').*')
+result_lines = []
+for line in all_lines:
+    if re.fullmatch(pattern, line):
+        result_lines.append(line)
+
